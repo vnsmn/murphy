150e32b631fe26cf090045fc187b34c19964cfed
vnsmn <github>
2015-08-02 10:00:54 +0300

    --

---
+++ elementary.js
@@ -0,0 +1,377 @@
+var elementaryJson = {
+
+unit2 : {
+	name : "am/is/are(questions)",
+	phrase_path : "sound/phrases/basic/u1/",
+	words_path : "sound/words/br/",
+	data : [ {
+		phrase : "am_i_late_no_you_re_on_time",
+		source : "am_i_late_no_you_re_on_time.mp3",
+		words : [ "am", "i", "late", "you", "on" ],
+		others : [ "are_they_new.mp3", "how_are_your_parents_are_they_well.mp3" ]
+	},
+	{
+		phrase : "are_these_your_keys_yes_they_are",
+		source : "are_these_your_keys_yes_they_are.mp3",
+		words : [ "are", "these", "your", "they" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_they_new",
+		source : "are_they_new.mp3",
+		words : [ "are", "they" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_you_hungry_no_i_m_not_but_i_m_thirsty",
+		source : "are_you_hungry_no_i_m_not_but_i_m_thirsty.mp3",
+		words : [ "are", "you", "i", "m", "not", "but" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_your_parent_s_at_home_no_they_re_out",
+		source : "are_your_parent_s_at_home_no_they_re_out.mp3",
+		words : [ "are", "your", "s", "at", "home", "they" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_your_shoes_new",
+		source : "are_your_shoes_new.mp3",
+		words : [ "are", "your" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_you_tired_yes_i_am",
+		source : "are_you_tired_yes_i_am.mp3",
+		words : [ "are", "you", "i" ],
+		others : [  ]
+	},
+	{
+		phrase : "how_are_your_parents_are_they_well",
+		source : "how_are_your_parents_are_they_well.mp3",
+		words : [ "are", "your", "they" ],
+		others : [  ]
+	},
+	{
+		phrase : "how_much_are_these_postcards_fifty_pence",
+		source : "how_much_are_these_postcards_fifty_pence.mp3",
+		words : [ "much", "are", "these" ],
+		others : [  ]
+	},
+	{
+		phrase : "how_old_is_joe_he_s_24",
+		source : "how_old_is_joe_he_s_24.mp3",
+		words : [ "old", "is", "he", "s" ],
+		others : [  ]
+	},
+	{
+		phrase : "how_s_your_father",
+		source : "how_s_your_father.mp3",
+		words : [ "s", "your" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_it_cold_in_your_room_yes_a_little",
+		source : "is_it_cold_in_your_room_yes_a_little.mp3",
+		words : [ "is", "it", "in", "your", "a" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_she_at_home",
+		source : "is_she_at_home.mp3",
+		words : [ "is", "she", "at" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_your_friend_english_yes_he_is",
+		source : "is_your_friend_english_yes_he_is.mp3",
+		words : [ "is", "your", "friend", "english", "he" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_your_mother_at_home",
+		source : "is_your_mother_at_home.mp3",
+		words : [ "is", "your", "mother", "at" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_your_mother_at_home_no_she_s_out",
+		source : "is_your_mother_at_home_no_she_s_out.mp3",
+		words : [ "is", "your", "mother", "at", "home", "she", "s" ],
+		others : [  ]
+	},
+	{
+		phrase : "that_s_my_seat_no_it_isn_t",
+		source : "that_s_my_seat_no_it_isn_t.mp3",
+		words : [ "that", "s", "my", "it" ],
+		others : [  ]
+	},
+	{
+		phrase : "what_colour_is_your_car_it_s_red",
+		source : "what_colour_is_your_car_it_s_red.mp3",
+		words : [ "what", "is", "your", "car", "it", "s" ],
+		others : [  ]
+	},
+	{
+		phrase : "what_s_the_time",
+		source : "what_s_the_time.mp3",
+		words : [ "what", "s", "the" ],
+		others : [  ]
+	},
+	{
+		phrase : "where_are_you_from_canada",
+		source : "where_are_you_from_canada.mp3",
+		words : [ "are", "you", "from" ],
+		others : [  ]
+	},
+	{
+		phrase : "where_is_your_mother_is_she_at_home",
+		source : "where_is_your_mother_is_she_at_home.mp3",
+		words : [ "is", "your", "mother", "she", "at" ],
+		others : [  ]
+	},
+	{
+		phrase : "where_s_jill",
+		source : "where_s_jill.mp3",
+		words : [ "s" ],
+		others : [  ]
+	},
+	{
+		phrase : "who_s_that_man",
+		source : "who_s_that_man.mp3",
+		words : [ "who", "s", "that" ],
+		others : [  ]
+	},
+	{
+		phrase : "why_are_you_angry",
+		source : "why_are_you_angry.mp3",
+		words : [ "are", "you" ],
+		others : [  ]
+	},
+	{
+		phrase : "your_shoes_are_nice_are_they_new",
+		source : "your_shoes_are_nice_are_they_new.mp3",
+		words : [ "your", "are", "they" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_these_your_keys_yes_they_are",
+		source : "are_these_your_keys_yes_they_are.mp3",
+		words : [ "are", "these", "your", "they" ],
+		others : [  ]
+	} ]
+},
+unit3 : {
+	name : "I am doing (present continuous)",
+	phrase_path : "sound/phrases/basic/u3/",
+	words_path : "sound/words/br/",
+	data : [ {
+		phrase : "can_you_phone_again_later",
+		source : "can_you_phone_again_later.mp3",
+		words : [ "can", "you", "again" ],
+		others : [  ]
+	},
+	{
+		phrase : "she_s_wearing_her_new_hat",
+		source : "she_s_wearing_her_new_hat.mp3",
+		words : [ "she", "s", "wearing", "her", "new" ],
+		others : [  ]
+	},
+	{
+		phrase : "the_weather_is_nice_at_the_moment",
+		source : "the_weather_is_nice_at_the_moment.mp3",
+		words : [ "the", "is", "at" ],
+		others : [  ]
+	},
+	{
+		phrase : "they_re_playing_in_the_park",
+		source : "they_re_playing_in_the_park.mp3",
+		words : [ "they", "in", "the" ],
+		others : [  ]
+	},
+	{
+		phrase : "we_re_having_dinner_now",
+		source : "we_re_having_dinner_now.mp3",
+		words : [ "we", "having" ],
+		others : [  ]
+	},
+	{
+		phrase : "where_are_the_children",
+		source : "where_are_the_children.mp3",
+		words : [ "are", "the" ],
+		others : [  ]
+	} ]
+},
+unit4 : {
+	name : "are you doing? (present continuous questions)",
+	phrase_path : "sound/phrases/basic/u4/",
+	words_path : "sound/words/br/",
+	data : [ {
+		phrase : "are_you_feeling_ok",
+		source : "are_you_feeling_ok.mp3",
+		words : [ "are", "you", "feeling" ],
+		others : [  ]
+	},
+	{
+		phrase : "are_your_friends_staying_at_a_hotel",
+		source : "are_your_friends_staying_at_a_hotel.mp3",
+		words : [ "are", "your", "staying", "at", "a" ],
+		others : [  ]
+	},
+	{
+		phrase : "her_parents_live_in_scotland",
+		source : "her_parents_live_in_scotland.mp3",
+		words : [ "her", "live", "in" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_it_raining",
+		source : "is_it_raining.mp3",
+		words : [ "is", "it" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_it_raining_yes_take_an_umbrella",
+		source : "is_it_raining_yes_take_an_umbrella.mp3",
+		words : [ "is", "it", "take", "an" ],
+		others : [  ]
+	},
+	{
+		phrase : "is_paul_working_today",
+		source : "is_paul_working_today.mp3",
+		words : [ "is", "working" ],
+		others : [  ]
+	},
+	{
+		phrase : "it_rains_a_lot_in_winter",
+		source : "it_rains_a_lot_in_winter.mp3",
+		words : [ "it", "a", "lot", "in" ],
+		others : [  ]
+	},
+	{
+		phrase : "it_s_not_cold",
+		source : "it_s_not_cold.mp3",
+		words : [ "it", "s", "not" ],
+		others : [  ]
+	},
+	{
+		phrase : "i_work_in_a_shop_my_brother_works_in_a_bank",
+		source : "i_work_in_a_shop_my_brother_works_in_a_bank.mp3",
+		words : [ "i", "work", "in", "a", "shop", "my", "brother", "works" ],
+		others : [  ]
+	},
+	{
+		phrase : "look_there_s_sally",
+		source : "look_there_s_sally.mp3",
+		words : [ "look", "there", "s" ],
+		others : [  ]
+	},
+	{
+		phrase : "no_they_aren_t_they_re_staying_with_me",
+		source : "no_they_aren_t_they_re_staying_with_me.mp3",
+		words : [ "they", "t", "staying", "with" ],
+		others : [  ]
+	},
+	{
+		phrase : "what_are_the_children_doing",
+		source : "what_are_the_children_doing.mp3",
+		words : [ "what", "are", "the", "children" ],
+		others : [  ]
+	},
+	{
+		phrase : "what_s_paul_doing_he_s_reading_the_newspaper",
+		source : "what_s_paul_doing_he_s_reading_the_newspaper.mp3",
+		words : [ "what", "s", "doing", "he", "reading", "the" ],
+		others : [  ]
+	},
+	{
+		phrase : "where_s_she_going",
+		source : "where_s_she_going.mp3",
+		words : [ "s", "she" ],
+		others : [  ]
+	},
+	{
+		phrase : "who_are_you_waiting_for_are_you_waiting_for_sue",
+		source : "who_are_you_waiting_for_are_you_waiting_for_sue.mp3",
+		words : [ "who", "are", "you", "for" ],
+		others : [  ]
+	},
+	{
+		phrase : "w_hy_are_you_w_earing_a_coat",
+		source : "w_hy_are_you_w_earing_a_coat.mp3",
+		words : [ "are", "you", "a" ],
+		others : [  ]
+	},
+	{
+		phrase : "yes_i_m_fine_thank_you",
+		source : "yes_i_m_fine_thank_you.mp3",
+		words : [ "i", "m", "fine" ],
+		others : [  ]
+	} ]
+},
+unit5 : {
+	name : "I do/work/like etc. (present simple)",
+	phrase_path : "sound/phrases/basic/u5/",
+	words_path : "sound/words/br/",
+	data : [ {
+		phrase : "it_costs_a_lot_of_money_to_stay_at_luxury_hotels",
+		source : "it_costs_a_lot_of_money_to_stay_at_luxury_hotels.mp3",
+		words : [ "it", "a", "lot", "of", "money", "to", "stay", "at", "luxury" ],
+		others : [  ]
+	},
+	{
+		phrase : "i_usually_go_to_work_by_car_but_sometimes_i_walk",
+		source : "i_usually_go_to_work_by_car_but_sometimes_i_walk.mp3",
+		words : [ "i", "go", "to", "work", "by", "car", "but", "sometimes" ],
+		others : [  ]
+	},
+	{
+		phrase : "john_has_a_shower_everyday",
+		source : "john_has_a_shower_everyday.mp3",
+		words : [ "has", "a", "shower" ],
+		others : [  ]
+	},
+	{
+		phrase : "julia_never_eats_breakfast",
+		source : "julia_never_eats_breakfast.mp3",
+		words : [  ],
+		others : [  ]
+	},
+	{
+		phrase : "sue_always_arrives_at_work_early",
+		source : "sue_always_arrives_at_work_early.mp3",
+		words : [ "always", "arrives", "at", "work" ],
+		others : [  ]
+	},
+	{
+		phrase : "the_earth_goes_round_the_sun",
+		source : "the_earth_goes_round_the_sun.mp3",
+		words : [ "the", "earth", "goes", "round" ],
+		others : [  ]
+	},
+	{
+		phrase : "the_shops_open_at_9_o_clock_and_close_at_5_30",
+		source : "the_shops_open_at_9_o_clock_and_close_at_5_30.mp3",
+		words : [ "the", "at", "o", "and" ],
+		others : [  ]
+	},
+	{
+		phrase : "tim_works_very_hard_he_starts_at_7_30_and_finishes_at_8_o_clock_in_the_evening",
+		source : "tim_works_very_hard_he_starts_at_7_30_and_finishes_at_8_o_clock_in_the_evening.mp3",
+		words : [ "works", "very", "hard", "he", "starts", "at", "and", "finishes", "o", "in", "the" ],
+		others : [  ]
+	},
+	{
+		phrase : "tom_lives_near_us_we_often_see_him",
+		source : "tom_lives_near_us_we_often_see_him.mp3",
+		words : [ "lives", "near", "us", "we", "often", "see" ],
+		others : [  ]
+	},
+	{
+		phrase : "we_do_a_lot_of_different_things_in_our_free_time",
+		source : "we_do_a_lot_of_different_things_in_our_free_time.mp3",
+		words : [ "we", "do", "a", "lot", "of", "different", "in", "our", "free" ],
+		others : [  ]
+	} ]
+}
+}
---
+++ intermedia.js
@@ -0,0 +1,14 @@
+var intermediaJson = {
+	unit1 : {
+		name : "I am",
+		phrase_path : "sound/murphy_smalzer/basic/u1/",
+		words_path : "/home/vns/gtranslator-dictionary/sounds/br/",
+		data : [ {
+			phrase : "I'm cold can you close the window please?",
+			source : "i_m_cold_can_you_close_the_window_please.mp3",
+			words : [ "i", "am", "learning", "english" ],
+			others : [ "i_m_cold_can_you_close_the_window_please" ]
+
+		} ]
+	}	
+};
